f6b24c41799ff87c4349200b2179b3939450e248
1bastien1 <macgrey0000@gmail.com>
2022-03-30 23:38:43 +0200

    add restaurants controller

--- src/routes/restaurants.js
+++ src/routes/restaurants.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
+var RestaurantController = require('../controllers/restaurants.controller')
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a restaurant');
+/* GET all restaurant listing. */
+router.get('/', RestaurantController.getAll, () => {
+  res.send(res);
 });
+
+/* GET one restaurant listing. */
+router.get('/:id', RestaurantController.getOne, () => {
+  res.send(res);
+});
+
+/* POST restaurant listing. */
+router.get('/', RestaurantController.createOne, () => {
+  res.send(res);
+});
+
+/* UPDATE restaurant listing. */
+router.get('/', RestaurantController.updateOne, () => {
+  res.send(res);
+});
+
+/* DELETE restaurant listing. */
+router.get('/', RestaurantController.deleteOne, () => {
+  res.send(res);
+});
+
+
 
 module.exports = router;
