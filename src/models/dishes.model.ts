588646f839f8b2e78c934e04cbe2865947cc82aa
1bastien1 <macgrey0000@gmail.com>
2022-03-31 19:51:58 +0200

    update dishes model

--- src/models/dishes.model.ts
+++ src/models/dishes.model.ts
@@ -1,8 +1,15 @@
 const mongoose = require('mongoose')
 
 const DishesSchema = new mongoose.Schema({
-    name: String,
-    price: Number,
+    name: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    currency: {type: String, required: true, enum: ['euro', 'dollar'] },
     allergens: [String],
     ingredients: [String],
     //pictures: any,
