3eff2630a1ca838d557f8a6d16642dc0bac382af
1bastien1 <macgrey0000@gmail.com>
2022-04-04 20:21:46 +0200

    add datas

---
+++ data-collection.js
@@ -0,0 +1,23 @@
+const restaurant = {
+    "name":"testResto",
+    "addr":"4 rue de l'ange",
+    "cp":"54111",
+    "city":"bebertvillier"
+ };
+
+ const review = {
+    "comments": "très bon plat",
+    "idDishe": "000000",
+    "note": "7",
+    "noteTaste": "10",
+    "noteQP": "8",
+    "author": "admin"
+  }
+
+  const dishe = {
+    "name":"burger",
+    "price":8.50,
+    "currency": "euro",
+    "allergens":["blé", "levure", "gluten"],
+    "ingredients":["salade", "tomate", "oignion"]
+ }
