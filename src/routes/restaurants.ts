48d84a80033e510e94d5863cdbbdf1e45c711ec0
1bastien1 <macgrey0000@gmail.com>
2022-04-04 22:40:10 +0200

    fix ECMA6

---
+++ src/routes/restaurants.ts
@@ -0,0 +1,33 @@
+import Express from 'express';
+import {RestaurantsController} from '../controllers/';
+var router = Express.Router();
+
+/* GET all restaurant listing. */
+router.get('/', RestaurantsController.getAll, (req, res) => {
+  res.send(res);
+});
+
+/* GET one restaurant listing. */
+router.get('/:id', RestaurantsController.getOne, (req, res) => {
+  res.send(res);
+});
+
+/* POST restaurant listing. */
+router.post('/', RestaurantsController.createOne, (req, res) => {
+  res.send(res);
+});
+
+/* UPDATE restaurant listing. */
+//TODO post = update?
+router.post('/:id', RestaurantsController.updateOne, (req, res) => {
+  res.send(res);
+});
+
+/* DELETE restaurant listing. */
+router.delete('/:id', RestaurantsController.deleteOne, (req, res) => {
+  res.send(res);
+});
+
+
+
+module.exports = router;
