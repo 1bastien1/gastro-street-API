b4fc32745119decb2ced0ade237f46e54aaba702
1bastien1 <macgrey0000@gmail.com>
2022-04-05 21:51:10 +0200

    WIP: run server with TS on index routes only

--- app.ts
+++ app.ts
@@ -1,73 +1,84 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+// const express = require('express');
+import express from 'express';
+// const path = require('path');
+import path from 'path';
+// const cookieParser = require('cookie-parser');
+import cookieParser from 'cookie-parser';
+// const logger = require('morgan');
+import logger from 'morgan';
 
-var indexRouter = require('./src/routes/index');
-var reviewsRouter = require('./src/routes/reviews');
-var restaurantsRouter = require('./src/routes/restaurants');
-var dishesRouter = require('./src/routes/dishes');
+// const debug = require('debug')('gastrostreet:server');
+import debug from 'debug';
+debug('gastrostreet:server');
+// const http = require('http');
+import http from 'http'
+// const indexRouter = require('./src/routes/index');
+import indexRouter from './src/routes/index';
 
-var dotenv  = require('./src/middleware/dotenv');
-var db = require('./src/middleware/database')
+// TODO
+// const reviewsRouter = require('./src/routes/reviews');
+// const restaurantsRouter = require('./src/routes/restaurants');
+// const dishesRouter = require('./src/routes/dishes');
+// const dotenv  = require('./src/middleware/dotenv');
+// const db = require('./src/middleware/database')
 
-var app = express();
+const app = express();
 
-var debug = require('debug')('gastrostreet:server');
-var http = require('http');
- 
- function normalizePort(val) {
-   var port = parseInt(val, 10);
- 
+ function normalizePort(val: any) {
+   // tslint:disable-next-line: no-shadowed-variable
+   const port = parseInt(val, 10);
+
    if (isNaN(port)) {
      // named pipe
      return val;
    }
- 
+
    if (port >= 0) {
      // port number
      return port;
    }
- 
+
    return false;
  }
- 
+
  /**
   * Event listener for HTTP server "error" event.
   */
- function onError(error) {
+ function onError(error: any) {
    if (error.syscall !== 'listen') {
      throw error;
    }
- 
-   var bind = typeof port === 'string'
+
+   const bind = typeof port === 'string'
      ? 'Pipe ' + port
      : 'Port ' + port;
- 
+
    // handle specific listen errors with friendly messages
    switch (error.code) {
      case 'EACCES':
-       console.error(bind + ' requires elevated privileges');
+       // console.error(bind + ' requires elevated privileges');
        process.exit(1);
        break;
      case 'EADDRINUSE':
-       console.error(bind + ' is already in use');
+       // console.error(bind + ' is already in use');
        process.exit(1);
        break;
      default:
        throw error;
    }
  }
- 
+
  /**
   * Event listener for HTTP server "listening" event.
   */
  function onListening() {
-   var addr = server.address();
-   var bind = typeof addr === 'string'
-     ? 'pipe ' + addr
-     : 'port ' + addr.port;
-   debug('Listening on ' + bind);
+  //  const addr = server.address();
+  //  const bind = typeof addr === 'string'
+  //    ? 'pipe ' + addr
+  //    : 'port ' + addr.port;
+   debug('Listening on ' + '3000');
+   // tslint:disable-next-line: no-console
+   console.log('on listenning')
  }
 
 
@@ -75,25 +86,27 @@ var http = require('http');
 /**
  * Get port from environment and store in Express.
  */
- var port = normalizePort(process.env.PORT || '3000');
+ const port = normalizePort(process.env.PORT || '3000');
  app.set('port', port);
- 
+
  /**
   * Create HTTP server.
   */
- var server = http.createServer(app);
- 
+ const server = http.createServer(app);
+
  /**
   * Listen on provided port, on all network interfaces.
   */
+ // tslint:disable-next-line: no-console
+ console.log('before listening')
  server.listen(port);
  server.on('error', onError);
  server.on('listening', onListening);
 
 
-//Middleware
-app.use(dotenv.config);
-app.use(db.connectToDB)
+// Middleware
+// app.use(dotenv.config);
+// app.use(db.connectToDB)
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -102,9 +115,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/reviews', reviewsRouter);
-app.use('/restaurants', restaurantsRouter);
-app.use('/dishes', dishesRouter);
+// app.use('/reviews', reviewsRouter);
+// app.use('/restaurants', restaurantsRouter);
+// app.use('/dishes', dishesRouter);
 
 
 module.exports = app;
