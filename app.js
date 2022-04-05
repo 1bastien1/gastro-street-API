7afe7bc60db04e6cacb44f78b0fb4f1ff6e00dab
1bastien1 <macgrey0000@gmail.com>
2022-04-05 16:04:56 +0200

    fix comments

--- app.js
+++ app.js
@@ -35,7 +35,6 @@ var http = require('http');
  /**
   * Event listener for HTTP server "error" event.
   */
- 
  function onError(error) {
    if (error.syscall !== 'listen') {
      throw error;
@@ -63,7 +62,6 @@ var http = require('http');
  /**
   * Event listener for HTTP server "listening" event.
   */
- 
  function onListening() {
    var addr = server.address();
    var bind = typeof addr === 'string'
@@ -77,37 +75,25 @@ var http = require('http');
 /**
  * Get port from environment and store in Express.
  */
-
  var port = normalizePort(process.env.PORT || '3000');
  app.set('port', port);
  
  /**
   * Create HTTP server.
   */
- 
  var server = http.createServer(app);
  
  /**
   * Listen on provided port, on all network interfaces.
   */
- 
  server.listen(port);
  server.on('error', onError);
  server.on('listening', onListening);
 
 
-
-
-
-
-
-
-
 //Middleware
 app.use(dotenv.config);
 app.use(db.connectToDB)
-
-
 
 app.use(logger('dev'));
 app.use(express.json());
