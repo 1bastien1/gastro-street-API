f37b8c6c52f7607d13ecac55db48de890215c4b7
1bastien1 <macgrey0000@gmail.com>
2022-04-06 19:39:12 +0200

    add common export

---
+++ src/types/index.ts
@@ -0,0 +1,8 @@
+// Request interface - override Express.request
+export * as commonTypeRequest from './commonRequestType'
+export * as disheType from './requestTypeDishe'
+export * as restaurantType from './requestTypeRestaurant'
+export * as reviewType from './requestTypeReview'
+
+// Response interface - override Express.response
+export * as commonTypeResponse from './commonResponseType'
