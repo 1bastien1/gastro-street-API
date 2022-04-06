890ef84c33615e4c3eeb5f3fb0f3d075f56d7424
1bastien1 <macgrey0000@gmail.com>
2022-04-06 19:44:04 +0200

    add all entities on same file

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,8 +1,5 @@
 // Request interface - override Express.request
 export * as commonTypeRequest from './commonRequestType'
-export * as disheType from './requestTypeDishe'
-export * as restaurantType from './requestTypeRestaurant'
-export * as reviewType from './requestTypeReview'
 
 // Response interface - override Express.response
 export * as commonTypeResponse from './commonResponseType'
