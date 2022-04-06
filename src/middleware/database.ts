b0703446c1dba91321393002dad5ef0b1e6be80c
1bastien1 <macgrey0000@gmail.com>
2022-04-06 18:38:11 +0200

    remove log

--- src/middleware/database.ts
+++ src/middleware/database.ts
@@ -3,7 +3,5 @@ import Mongoose from 'mongoose'
 
 export const connectToDB = (_req: Express.Request, _res: Express.Response, next: NextFunction) => {
     Mongoose.connect(process.env.DB_URL || '')
-    .then(() => console.log('connect to db'))
-    .catch((err) => console.log(Error(err)))
     next();
 }
