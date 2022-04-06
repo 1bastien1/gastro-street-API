9970e4e0f44ae0fe2a83773f88cecd5123ef91cb
1bastien1 <macgrey0000@gmail.com>
2022-04-06 19:39:31 +0200

    change req,res type dishe

--- src/controllers/dishes.controller.ts
+++ src/controllers/dishes.controller.ts
@@ -1,35 +1,37 @@
 import {DishesModel} from '../models'
+import { commonTypeResponse, disheType } from '../types'
+import { Dishe } from '../types/requestTypeDishe'
 
 /**
  * retreive all restaurants
  * @param {*} req
  * @param {*} res
  */
-export const getAll = ((req: any, res: any) => {
+export const getAll = ((req: disheType.Express.RequestGet, res: commonTypeResponse.Express.ResponseGet<Dishe>) => {
     DishesModel.find({})
         .then(result => res.status(200).json({ result }))
         .catch(error => res.status(500).json({msg: error}))
 })
 
-export const getOne = ((req: any, res: any) => {
+export const getOne = ((req: disheType.Express.RequestGet, res: commonTypeResponse.Express.ResponseGet<Dishe>) => {
     DishesModel.findOne({ _id: req.params.id })
         .then(result => res.status(200).json({ result }))
         .catch(() => res.status(404).json({msg: 'Dishe not found'}))
 })
 
-export const createOne = ((req: any, res: any) => {
+export const createOne = ((req: disheType.Express.RequestPost, res: commonTypeResponse.Express.ResponseGet<Dishe>) => {
     DishesModel.create(req.body)
         .then(result => res.status(200).json({ result }))
         .catch((error) => res.status(500).json({msg:  error }))
 })
 
-export const updateOne = ((req: any, res: any) => {
+export const updateOne = ((req: disheType.Express.RequestPost, res: commonTypeResponse.Express.ResponseGet<Dishe>) => {
     DishesModel.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true, runValidators: true })
         .then(result => res.status(200).json({ result }))
         .catch((error) => res.status(404).json({msg: 'Dishe not found' }))
 })
 
-export const deleteOne = ((req: any, res: any) => {
+export const deleteOne = ((req: disheType.Express.RequestGet, res: commonTypeResponse.Express.ResponseGet<Dishe>) => {
     DishesModel.findOneAndDelete({ _id: req.params.id })
         .then(result => res.status(200).json({ result }))
         .catch((error) => res.status(404).json({msg: 'Dishe not found' }))
