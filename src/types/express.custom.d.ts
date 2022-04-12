0b4b3705cdee6cd974dbde7c82b6657c8c65bc38
1bastien1 <macgrey0000@gmail.com>
2022-04-12 23:37:39 +0200

    add custom type def express (req,res)

---
+++ src/types/express.custom.d.ts
@@ -0,0 +1,32 @@
+export declare namespace Express {
+    interface RequestGet {
+        params: {
+            id: number,
+        };
+    }
+    interface RequestPost<T> extends RequestGet {
+        body: T;
+    }
+}
+
+/**
+ * common response interface used to declare all common fn on every response
+ */
+ interface CommonResponse {
+    status: (httpCode: number) => CommonResponse;
+    json: (rawData: any) => CommonResponse;
+}
+
+
+/**
+ * see https://expressjs.com/fr/api.html#res for furthr information
+ */
+// tslint:disable-next-line: no-namespace
+export declare namespace Express {
+    interface ResponseGet<T> extends CommonResponse {
+        result: T | T[];
+    }
+    interface ResponsePost<T> extends CommonResponse {
+        result: T;
+    }
+}
