1da42d39adabfe5c3661d6b82798167273ae30f1
bastienJanon <bastien.janon@jvs.fr>
2024-01-01 22:50:09 +0100

    remove useless call dotenv

--- src/middleware/dotenv.ts
+++ src/middleware/dotenv.ts
@@ -1,8 +1,6 @@
 import dotenv from 'dotenv'
 import { NextFunction, RequestHandler } from 'express';
 
-// dotenv.config({path: '.env-credential'});
-
 export const config: RequestHandler =  (req: Express.Request, res: Express.Response, next: NextFunction) => {
     dotenv.config({path: '.env-credential'});
     next();
