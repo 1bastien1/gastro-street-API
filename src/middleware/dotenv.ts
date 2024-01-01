0d527990eeb3b3193820f9edffb1f65b062dfb54
bastienJanon <bastien.janon@jvs.fr>
2024-01-01 22:36:42 +0100

    comments

--- src/middleware/dotenv.ts
+++ src/middleware/dotenv.ts
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv'
 import { NextFunction, RequestHandler } from 'express';
 
-dotenv.config({path: '.env-credential'});
+// dotenv.config({path: '.env-credential'});
 
 export const config: RequestHandler =  (req: Express.Request, res: Express.Response, next: NextFunction) => {
     dotenv.config({path: '.env-credential'});
